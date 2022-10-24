a733c634e168e7ea8a64cee7e4f022a9636e6e56
Wulan <ASUS@LAPTOP-781R3HVH>
2022-10-24 16:52:22 +0700

    loop to search for the elements in the array

--- Searchalgorithm/Program.cs
+++ Searchalgorithm/Program.cs
@@ -67,7 +67,16 @@ namespace Searchalgorithm
                     mid = (lowerboand + upperboand) / 2;
                     ctr++;
                 }
-            }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + " found at position " + (mid = 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + " not found in the array\n");
+                Console.WriteLine("\nNumber of comparasion : " + ctr);
+
+                Console.Write("\nContinue search (y/n):");
+                ch = char.Parse(Console.ReadLine());
+
+            } while ((ch == "y") || (ch == "Y"));
         }
         static void Main(string[] args)
         {
