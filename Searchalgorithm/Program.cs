3902769a5a3dd9603a19960dc88fc28b4753c53f
Wulan <ASUS@LAPTOP-781R3HVH>
2022-10-24 16:11:45 +0700

    number off element in the array

--- Searchalgorithm/Program.cs
+++ Searchalgorithm/Program.cs
@@ -10,6 +10,8 @@ namespace Searchalgorithm
     {
         //array to be searchd
         int[] arr = new int[20];
+        //number off element in the array
+        int n;
         static void Main(string[] args)
         {
         }
