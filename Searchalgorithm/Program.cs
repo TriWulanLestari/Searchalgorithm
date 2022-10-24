773814d4e98abb6c50fe1d44c06f5961a3de7de0
Wulan <ASUS@LAPTOP-781R3HVH>
2022-10-24 16:10:26 +0700

    array to be searchd

--- Searchalgorithm/Program.cs
+++ Searchalgorithm/Program.cs
@@ -8,6 +8,8 @@ namespace Searchalgorithm
 {
     internal class Program
     {
+        //array to be searchd
+        int[] arr = new int[20];
         static void Main(string[] args)
         {
         }
