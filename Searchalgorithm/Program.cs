5e47039f7e2bb0ecdcde2ef483ad5a3a390a728e
Wulan <ASUS@LAPTOP-781R3HVH>
2022-10-24 16:21:54 +0700

    Enter the number of elemnts in the array

--- Searchalgorithm/Program.cs
+++ Searchalgorithm/Program.cs
@@ -14,6 +14,20 @@ namespace Searchalgorithm
         int n;
         //Get the number of elements to store in the array
         int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elemnts in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements.\n");
+            }
+        }
         static void Main(string[] args)
         {
         }
